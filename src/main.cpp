1e7f35702bf409c5d9c6788bc75df6f047eaaa6a
hankotom <hankotamas@gmail.com>
2023-03-27 21:51:51 +0200

    Start code with timer bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,141 @@
 #include <Arduino.h>
 
+/*
+Kétkörös fűtés vezérlés.
+Bemenet: A termosztát rövidzárja
+Kimenetek: Radiátor kör keringerő szivattyú és a kazán rövidzárja
+
+Állapotok: 
+Stop - itt mindenki áll
+Starting - jön egy indítójel, akkor először elindítja a radiátor szivattyút
+Work - Késleltetés után elindítja a kazánt is
+Stopping - leállítja a kazánt, de utána még x percig kerinteti a radiátorkört, majd azt is leállítja
+
+
+*/
+
+#define THERMOSTATON HIGH
+#define THERMOSTATOFF LOW
+
+#define PUMPON HIGH
+#define PUMPOFF LOW
+
+#define BOILERON HIGH
+#define BOILEROFF LOW
+
+// constants won't change. Used here to set a pin number:
+
+const int ledPin =  13;      // the number of the LED pin
+const int thermostatPin =  2;// the number of the LED pin
+const int boilerPin =  10;// the number of the LED pin
+const int pumpPin =  10;// the number of the LED pin
+
+// Variables will change:
+int thermostat = 0;         // variable for reading the thermostat status
+int pump = PUMPOFF;         // variable for pump status
+int boiler = BOILEROFF;         // variable for boiler status
+enum states{Stop, Starting, Work, Stopping} HeatState;
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long TimerForStart = 0;        // will store last time LED was updated
+unsigned long TimerForStop = 0;        // will store last time LED was updated
+
+// constants won't change:
+const unsigned long StartDelay = 1000;           // 
+const unsigned long StopDelay  = 2000;           // 
+
 void setup() {
-  // put your setup code here, to run once:
+  // set the digital pin as output:
+  pinMode(thermostatPin, INPUT);
+  pinMode(pumpPin, OUTPUT);
+  pinMode(boilerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Goodnight moon!");
+
+  boiler = BOILEROFF;
+  pump = PUMPOFF;
+  HeatState = Stop;
+
+  previousMillis = millis();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+  if (previousMillis + 1 <= millis())   // check onli in every milisecond
+  {
+    previousMillis = millis();
+    thermostat = digitalRead(thermostatPin);
+
+    switch (HeatState){
+
+      case Stop:
+        boiler = BOILEROFF;
+        pump = PUMPOFF;
+        if(thermostat == THERMOSTATON){
+          HeatState = Starting;
+          TimerForStart = 0;
+        }
+        break;
+
+      case Starting:
+        boiler = BOILEROFF;
+        pump = PUMPON;
+        if(thermostat == THERMOSTATOFF){
+          HeatState = Stop;
+        }
+        if((thermostat == THERMOSTATON) && (TimerForStart++ >= StartDelay)){
+          HeatState = Work;
+        }
+        break;
+
+      case Work:
+        boiler = BOILERON;
+        pump = PUMPON;
+        if(thermostat == THERMOSTATOFF){
+          HeatState = Stopping;
+          TimerForStop = 0;
+        }
+        break;
+
+      case Stopping:
+        boiler = BOILEROFF;
+        pump = PUMPON;
+
+        if(thermostat == THERMOSTATON){
+          HeatState = Work;
+        }
+
+        if((thermostat == THERMOSTATOFF) && (TimerForStop++ >= StopDelay)){
+          HeatState = Stop;
+        }
+
+        break;
+
+      default:
+        boiler = BOILEROFF;
+        pump = PUMPOFF;
+        HeatState = Stop;
+        break;
+    }
+    digitalWrite(boilerPin, boiler);
+    digitalWrite(pumpPin, pump);
+    if ((millis() % 400) == 0){
+      Serial.println(millis());
+      Serial.print("Kazan: ");Serial.print(boiler); Serial.print(" Szivattyu: ");Serial.println(pump);
+    }
+
+  }
+//  if ((millis() % 400) == 0)
+//    digitalWrite(ledPin, !ledPin);
+//  Serial.println(millis() % 400);  //   Serial.print("  ");   Serial.println(pump);
+
 }
